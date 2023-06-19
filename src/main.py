33781a3c6317d30f3ccc51e91c911b2b79e09745
Andrejgas <andrejgas1@gmail.com>
2023-06-19 23:49:40 +0300

    добавлен суперцикл в main.py

--- src/main.py
+++ src/main.py
@@ -2,3 +2,37 @@
 Точка входа: Управляющий код.
 """
 
+# стандартная библиотека
+
+# проект
+import data
+
+#1. Чтение файлов данных.
+
+# 2. ЕСЛИ первый запуск:
+
+    # вывод раздела помощи
+
+
+# суперцикл
+while True:
+    # 4. Ожидание команды ввода
+    command = input(data.PROMPT)
+    if command in data.COMMANDS['начать новую партию']:
+        ...
+    elif command in data.COMMANDS['загрузить существующую партию']:
+        ...
+    elif command in data.COMMANDS['отобразить раздел помощи']:
+        ...
+    elif command in data.COMMANDS['создать или переключиться на игрока']:
+        ...
+    elif command in data.COMMANDS['отобразить таблицу результатов']:
+        ...
+    elif command in data.COMMANDS['изменить размер поля']:
+        ...
+    elif command in data.COMMANDS['выйти']:
+        break
+
+    # !!!отобразить раздел помощи при вводе неправильной команды!!!
+
+# 16. обработка завершения приложения
