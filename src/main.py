24e639ecc1cc69a698fdc6d600a0d26b37c9a764
Andrejgas <andrejgas1@gmail.com>
2023-06-20 00:38:38 +0300

    условие при первом запуске

--- src/main.py
+++ src/main.py
@@ -6,11 +6,14 @@
 
 # проект
 import data
+import help
+import utils
 
 #1. Чтение файлов данных.
 
 # 2. ЕСЛИ первый запуск:
-
+if utils.read_players():
+    ...
     # вывод раздела помощи
 
 
@@ -18,18 +21,25 @@ import data
 while True:
     # 4. Ожидание команды ввода
     command = input(data.PROMPT)
+
     if command in data.COMMANDS['начать новую партию']:
         ...
+
     elif command in data.COMMANDS['загрузить существующую партию']:
         ...
+
     elif command in data.COMMANDS['отобразить раздел помощи']:
         ...
+
     elif command in data.COMMANDS['создать или переключиться на игрока']:
         ...
+
     elif command in data.COMMANDS['отобразить таблицу результатов']:
         ...
+
     elif command in data.COMMANDS['изменить размер поля']:
         ...
+
     elif command in data.COMMANDS['выйти']:
         break
 
