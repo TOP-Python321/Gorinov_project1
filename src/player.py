7a9e235c206e87a1250bc9d98e9d8b85c101d1cb
Andrejgas <andrejgas1@gmail.com>
2023-06-22 01:07:45 +0300

    добавлены анотации

--- src/player.py
+++ src/player.py
@@ -2,23 +2,32 @@
 Работа с данными игроков.
 """
 import data
-from utils import write_players
+import utils
+
 
 def name_input() -> str:
+    """
+    Запрашивает и проверяет имя пользователя на соответствие шаблону. Если имя корректное то
+     возвращает имя как объект str.
+    :return: str - имя пользователя
+    """
     while True:
         name = input(f' {data.MESSAGES["ввод имени"]}{data.PROMPT}')
         if data.NAME_PATTERN.fullmatch(name):
             return name
         print(f'{data.MESSAGES["некорректное имя"]}')
 
+
 def get_players_name() -> None:
-    """"""
+    """
+    Запрашивает имя игрока. Если имени нет в базе игроков, то добавляет имя в базу игроков и обновляет файлы данных.
+    :return: None
+    """
     name = name_input()
 
     if name not in data.players_db:
         data.players_db[name] = {'побед': 0, 'поражений': 0, 'ничьих': 0}
-        # !!!написать функцию!!!
-        write_players()
+        utils.write_players()
         # !!!написать вывод помощи!!!
         # help.full()
     data.players += [name]
@@ -32,5 +41,3 @@ def update_stats(players: list[str]):
         winner, looser = players
         data.players_db[winner]['побед'] += 1
         data.players_db[looser]['поражений'] += 1
-
-
