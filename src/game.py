5d91899330239962cfa2982dca587963560fda62
Andrejgas <andrejgas1@gmail.com>
2023-06-23 21:04:53 +0300

    редактирование game.game(): переписан print, добавлен return.

--- src/game.py
+++ src/game.py
@@ -7,7 +7,7 @@ import player
 
 def get_human_turn() -> int | None:
     while True:
-        turn = input(data.PROMPT)
+        turn = input(f"{data.MESSAGES['ход']}{data.PROMPT}")
         if not turn:
             return None
         try:
@@ -31,14 +31,16 @@ def game() -> list[str] | None:
             # save_game()
             return None
         data.turns[turn] = data.TOKENS[o]
-        data.field_coordinates = data.field_coordinates | data.turns
-        print(data.field_template.format(*data.field_coordinates.values()))
+        print(data.field_template.format(*(data.field_coordinates | data.turns).values()))
         if (
                 set(tuple(data.turns)[::2]) in data.winning_combinations or
                 set(tuple(data.turns)[1::2]) in data.winning_combinations
         ):
             print(f'выиграл игрок {data.players[o]}')
-
+            result_players = data.players[:]
+            if data.players[o] != data.players[0]:
+                result_players.reverse()
+            return result_players
     else:
         # ничья
         return []
